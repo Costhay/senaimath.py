c355ae97e5d9421d2f2030c8da38fa518c8af1d7
TIAGO SILVA SOUZA <39834308809@info.local>
2026-03-06 22:00:50 -0300

    Seno_Tiago

--- senaimath.py
+++ senaimath.py
@@ -1,1 +1,35 @@
-# math thaina
+
+anguloRecebido = float(input("Digite o ângulo em graus: "))
+
+
+pi = 3.141592653589793
+
+k = 10
+
+def radiano(angulo):
+    rad = angulo * pi / 180
+    return rad
+
+radianoConvertido = radiano(anguloRecebido)
+
+
+def funcao_taylor(rad, k):
+    sin = 0
+
+    for pedaco in range(k):
+        
+        fatorialAtual = 2 * pedaco + 1
+
+        fatorialCalc = 1
+        for i in range(1, fatorialAtual + 1):
+            fatorialCalc *= i
+
+        formula = (((-1) ** pedaco) * (rad ** (2 * pedaco + 1))) / fatorialCalc
+        sin += formula
+
+    return sin
+  
+
+resultado = funcao_taylor(radianoConvertido, k)
+print(f'O seno do angulo {anguloRecebido} = {resultado:.6f}')
+
